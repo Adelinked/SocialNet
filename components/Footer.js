041d7659b8b6e871fe03ab6f8ac9510aa164f46d
Adel Aziz Boulfekhar <boulfekhar.adelaziz@gmail.com>
2022-04-23 12:12:56 +0100

    Initial Release

---
+++ components/Footer.js
@@ -0,0 +1,26 @@
+import "font-awesome/css/font-awesome.min.css";
+
+export default function Footer() {
+  return (
+    <p style={{ fontWeight: "600", textAlign: "center" }}>
+      Designed and coded by Adelinked{" "}
+      <a
+        style={{ fontSize: "22px", marginRight: "5px" }}
+        href="https://github.com/Adelinked"
+        title="Git hub"
+        target="_blank"
+      >
+        <i class="fa fa-github"></i>
+      </a>
+      <a
+        style={{ fontSize: "22px", marginRight: "5px" }}
+        href="mailto:boulfekhar.adelaziz@gmail.com"
+        title="Send an email"
+        target="_blank"
+      >
+        <i class="fa fa-envelope"></i>
+      </a>
+      .
+    </p>
+  );
+}
