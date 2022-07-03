c1af537125616abc3b956c0f032c6b35b25aea69
Adel Aziz Boulfekhar <boulfekhar.adelaziz@gmail.com>
2022-07-03 21:36:29 +0100

    add time constants

---
+++ variables.js
@@ -0,0 +1,10 @@
+export const USER_REFRESH_STATUS = 300000; /* 5*60*1000 ie 5 mins */
+export const TIME_PROFILES_UPDATE = 300000; /* 5*60*1000 ie 5 mins */
+export const OLD_ONLINE_STATUS = 600000; /* 10*60*1000 ie 10 mins */
+
+export const TIME_REFRESH_POSTS = 120000; /* 2 mins */
+export const TIME_REFRESH_COMMENTS = 120000; /* 2 mins */
+export const TIME_REFRESH_POST_REACTIONS = 60000; /* 1 min */
+export const TIME_REFRESH_COMMENT_REACTIONS = 60000; /* 1 min */
+
+export const POSTS_LIMIT = 5; /* The number of posts fetched at the first render and each time scroll reach the bootom of the page  */
